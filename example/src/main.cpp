5764cf559aeb7f0e8312349b38c4b20142fd6d88
Andreas Huber <andreas.huber@infinite-coding.com>
2020-10-29 11:25:50 +0100

    Break C++ code on purpose to test if the build fails

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -3,7 +3,7 @@
 void setup() {
   // put your setup code here, to run once:
 }
-
+- Break C++ code on purpose to test if the build fails
 void loop() {
   // put your main code here, to run repeatedly:
 }
